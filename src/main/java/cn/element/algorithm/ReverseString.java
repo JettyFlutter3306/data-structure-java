1460dcc4e30603aa1064f001c1c97321a6bbeb75
Dell <1529146935@qq.com>
2021-05-19 12:44:32 +0800

    2021-5-19 12:40:00  实现了递归逆转字符串操作

--- src/main/java/cn/element/algorithm/ReverseString.java
+++ src/main/java/cn/element/algorithm/ReverseString.java
@@ -7,10 +7,34 @@ public class ReverseString {
 
     public static String reverseStr(String str){
 
-        return null;
+        char[] chars = str.toCharArray();
+
+        recurse(chars,0,chars.length - 1);
+
+        return new String(chars);
+    }
+
+    private static void recurse(char[] chars,int i,int j){
+
+        if(i > j){
+            return;
+        }
+
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
+
+        recurse(chars,i+1,j-1);
     }
 
     public static void main(String[] args) {
+
+        String temp = "abcdefg";
+
+        String s = reverseStr(temp);
+
+        System.out.println(s);
+
 
     }
 }
